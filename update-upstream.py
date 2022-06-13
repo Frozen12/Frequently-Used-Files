b854f2736630ae3153ee1ee13f7656821a4a4900
Frozen12 <31291110+Frozen12@users.noreply.github.com>
2022-06-13 12:18:51 +0600

    Update update-upstream.py

--- update-upstream.py
+++ update-upstream.py
@@ -1,5 +1,5 @@
 from logging import error as log_error, info as log_info
-from os import path as ospath, environ
+from os import path as ospath, environ,
 from subprocess import run as srun
 
 UPSTREAM_REPO = environ.get('UPSTREAM_REPO')
@@ -8,27 +8,26 @@ try:
     if len(UPSTREAM_REPO) == 0:
        raise TypeError
 except:
-    UPSTREAM_REPO = None
+    UPSTREAM_REPO = "https://github.com/anasty17/mirror-leech-telegram-bot"
 try:
     if len(UPSTREAM_BRANCH) == 0:
        raise TypeError
 except:
-    UPSTREAM_BRANCH = 'master'
+    UPSTREAM_BRANCH = 'main'
 
-if UPSTREAM_REPO is not None:
-    if ospath.exists('.git'):
-        srun(["rm", "-rf", ".git"])
+if ospath.exists('.git'):
+    srun(["rm", "-rf", ".git"])
 
-    update = srun([f"git init -q \
-                     && git config --global user.email frozen12@gmail.com \
-                     && git config --global user.name frozen12 \
-                     && git add . \
-                     && git commit -sm update -q \
-                     && git remote add origin {UPSTREAM_REPO} \
-                     && git fetch origin -q \
-                     && git reset --hard origin/{UPSTREAM_BRANCH} -q"], shell=True)
+update = srun([f"git init -q \
+                 && git config --global user.email frozen12@gmail.com \
+                 && git config --global user.name frozen12 \
+                 && git add . \
+                 && git commit -sm update -q \
+                 && git remote add origin {UPSTREAM_REPO} \
+                 && git fetch origin -q \
+                 && git reset --hard origin/{UPSTREAM_BRANCH} -q"], shell=True)
 
-    if update.returncode == 0:
-        log_info('Successfully updated with latest commit from UPSTREAM_REPO')
-    else:
-        log_error('Something went wrong while updating, check UPSTREAM_REPO if valid or not!')
+if update.returncode == 0:
+    log_info('Successfully updated with latest commit from UPSTREAM_REPO')
+else:
+    log_error('Something went wrong while updating, check UPSTREAM_REPO if valid or not!')
